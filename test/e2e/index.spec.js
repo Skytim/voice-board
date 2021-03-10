0ebb6c33c8f2a47081e8e572b3893ce5b99d20ff
Tim Tsai <tim.tsai@xuenn.com>
2021-03-10 12:16:08 +0800

    Jest parameter edit

---
+++ test/e2e/index.spec.js
@@ -0,0 +1,15 @@
+describe("VoiceBoard", () => {
+  beforeAll(async () => {
+    if (process.env.DEPLOY_ENV === "GH_PAGES") {
+      await page.goto("https://skytim.github.io/voice-board/");
+    } else {
+      await page.goto("http://localhost:3000/");
+    }
+  });
+  it('should be titled "VoiceBoard"', async () => {
+    await expect(page.title()).resolves.toMatch("VoiceBoard");
+  });
+  // it("Default Filter Button", async () => {
+  //   await expect(page.title()).resolves.toMatch("Google");
+  // });
+});
