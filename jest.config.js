9eea0c271dbb615b2f67e75640c01556185ed860
Tim Tsai <tim.tsai@xuenn.com>
2021-03-10 11:02:48 +0800

    jest puppeter ci init

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,21 @@
-module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
-  },
-  moduleFileExtensions: [
-    'js',
-    'vue',
-    'json'
-  ],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ],
-  preset: 'jest-puppeteer',
-}
+module.exports =
+  process.env.DEPLOY_ENV === "E2E"
+    ? { preset: "jest-puppeteer" }
+    : {
+        moduleNameMapper: {
+          "^@/(.*)$": "<rootDir>/$1",
+          "^~/(.*)$": "<rootDir>/$1",
+          "^vue$": "vue/dist/vue.common.js"
+        },
+        moduleFileExtensions: ["js", "vue", "json"],
+        transform: {
+          "^.+\\.js$": "babel-jest",
+          ".*\\.(vue)$": "vue-jest"
+        },
+        collectCoverage: true,
+        collectCoverageFrom: [
+          "<rootDir>/components/**/*.vue",
+          "<rootDir>/pages/**/*.vue"
+        ],
+        testPathIgnorePatterns:["/node_modules/", "<rootDir>/test/e2e/"]
+      };
